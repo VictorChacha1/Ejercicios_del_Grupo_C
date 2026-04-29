a5139f5ec77828c2cd9d5a342b8af9371b3962d6
Victor Chacha <guillo.123efra@gmail.com>
2026-04-29 08:17:14 -0500

    Update Ejercicio1_Crear_y_guardar.cpp

--- Ejercicio1_Crear_y_guardar.cpp
+++ Ejercicio1_Crear_y_guardar.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
 using namespace std;
 
 int main() {
+    string nombre, ciudad;
+    int edad;
+
+    cout << "Ingrese nombre : "; cin >> nombre;
+    cout << "Ingrese edad   : "; cin >> edad;
+    cout << "Ingrese ciudad : "; cin >> ciudad;
+
     ofstream archivo("datos.txt");
-    if (archivo.is_open()) {
-        archivo << "Juan" << " " << 20 << " " << "Quito" << endl;
-        archivo.close();
-        cout << "Archivo creado con exito." << endl;
+
+    if (!archivo.is_open()) {
+        cout << "ERROR: No se pudo crear el archivo." << endl;
+        return 1;
     }
+
+    // Guardamos los datos solicitados
+    archivo << "Nombre: " << nombre << endl;
+    archivo << "Edad  : " << edad << endl;
+    archivo << "Ciudad: " << ciudad << endl;
+
+    archivo.close(); // Cerramos el flujo[cite: 1]
+
+    cout << "\nArchivo 'datos.txt' creado exitosamente." << endl;
+
     return 0;
 }
