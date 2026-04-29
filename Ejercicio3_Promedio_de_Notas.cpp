6c51c204a88fdf66e29f1ef51ca14879df5e265b
Victor Chacha <guillo.123efra@gmail.com>
2026-04-29 07:57:05 -0500

    Update Ejercicio3_Promedio_de_Notas.cpp

--- Ejercicio3_Promedio_de_Notas.cpp
+++ Ejercicio3_Promedio_de_Notas.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    float nota, suma = 0, promedio;
+    for(int i = 1; i <= 5; i++) {
+        cout << "Ingrese nota " << i << ": ";
+        cin >> nota;
+        suma += nota;
+    }
+    promedio = suma / 5;
+    cout << "Promedio: " << promedio << " - Estado: ";
+    if (promedio >= 9) cout << "Excelente";
+    else if (promedio >= 7) cout << "Bueno";
+    else cout << "Regular";
+    return 0;
+}
